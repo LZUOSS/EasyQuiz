73c6eb45f6c446eb6230bf862a75bab0ada810e8
Bird <dstsmallbird@foxmail.com>
2015-11-05 10:49:27 +0000

    Fix bug
    Signed-off-by: Bird <dstsmallbird@foxmail.com>

--- proc.php
+++ proc.php
@@ -4,7 +4,7 @@ $name=$_GET[ 'name'];
 $phone=$_GET[ 'phone']; 
 $school=$_GET[ 'school'];
 $score=$_GET[ 'score' ]; 
-$msg="\n 姓名:" .$name. "\r\n ID:" .$id. "\r\n 手机号码:" .$phone. "\r\n 学院:" .$school. $score. "\r\n 分数：" .$score. "\r\n";
+$msg="\n 姓名:" .$name. "\r\n ID:" .$id. "\r\n 手机号码:" .$phone. "\r\n 学院:" .$school. "\r\n 分数：" .$score. "\r\n";
 file_put_contents("result.txt",$msg,FILE_APPEND); 
 ?>
 
